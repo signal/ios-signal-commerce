2780fe71541bdffc03a5edb6a28bbc68658b2338
Andrew Violette <aviolette@gmail.com>
2016-01-11 13:37:15 -0600

    [adhoc:ios-signal-commerce] initial commit

---
+++ Signal Commerce/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Signal Commerce
+//
+//  Created by Andrew on 1/11/16.
+//  Copyright © 2016 Signal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Signal Commerce/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  Signal Commerce
+//
+//  Created by Andrew on 1/11/16.
+//  Copyright © 2016 Signal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
