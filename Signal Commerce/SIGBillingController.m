57d829307e409ad67e72064f977b337dfaaa1bb1
Andrew Violette <aviolette@gmail.com>
2016-02-16 12:55:31 -0600

    Added activity indicator for purchase page

--- Signal Commerce/SIGBillingController.m
+++ Signal Commerce/SIGBillingController.m
@@ -10,11 +10,36 @@
 #import "SIGPurchaseCompleteController.h"
 #import "AppDelegate.h"
 
+@interface SIGBillingController()
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (weak, nonatomic) IBOutlet UIButton *purchaseButton;
+
+@end
+
 @implementation SIGBillingController
 
--(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    SIGPurchaseCompleteController *controller = segue.destinationViewController;
+-(void)viewDidLoad {
+    [_activityIndicator setHidden: YES];
+}
+
+- (IBAction)purchaseClick:(id)sender {
+    [_activityIndicator setHidden: NO];
+    [_activityIndicator startAnimating];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [NSThread sleepForTimeInterval:2];
+        [self performSelectorOnMainThread:@selector(launchPurchaseComplete) withObject:nil waitUntilDone:NO];
+    });
+}
+
+-(void)launchPurchaseComplete {
+    [_activityIndicator stopAnimating];
+    [_activityIndicator setHidden: YES];
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle: nil];
+    SIGPurchaseCompleteController *controller = [storyboard instantiateViewControllerWithIdentifier: @"PurchaseComplete"];
     controller.handoff = self.navigationController;
+    [self.navigationController presentViewController:controller animated:YES completion:^{
+        [_purchaseButton setHidden: YES];
+    }];
 }
 
 @end
